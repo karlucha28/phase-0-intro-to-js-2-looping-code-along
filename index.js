941ff50c76dfc9c2432b97e6d8f487619a2b8508
Karl Savinskiy <karlsavinskiy@gmail.com>
2022-07-03 09:21:01 -0700

    finished

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Olliex", "Aki"];
+const birthday = "birthday";
+const newArray =[];
+
+function writeCards(names, birthday) {
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+        
+    }
+    return newArray;
+}
+console.log(newArray);
+
+const number = "";
+function countDown(number){
+     let i = 10;
+    while (i >= 0) {
+        console.log(i);
+        i--
+    } 
+    return number;
+}
+countDown(number);
